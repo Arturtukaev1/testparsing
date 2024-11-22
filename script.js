dc798811041022c9fa6dbfc0323b9110f7f2913c
Artur <118470461+Arturtukaev1@users.noreply.github.com>
2024-11-22 21:46:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const form = document.getElementById('info-form');
 const thankYouMessage = document.getElementById('thank-you-message');
 
 // URL вашего веб-приложения Google Apps Script
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwGo9IdECmkcgwWs0Vx1FjhYJKV2AK0ljJK83MoyG4TvqEZ7aB28RFXZKy9RLueLtAbDw/exec'; // Замените на ваш URL
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwEjPUuqmLVh2l6ohkwqjhUo8G8OUTYTlrEnpEwmKiOkcjzzDrvfqFYHx7Y-ZOwjkBMdA/exec'; // Замените на ваш URL
 
 // Добавляем обработчик события отправки формы
 form.addEventListener('submit', function (event) {
